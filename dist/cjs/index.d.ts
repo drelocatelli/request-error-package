c8d2e773b499b2f64e1275e2a22298dfceb4698f
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2022-06-23 19:47:12 -0300

    Delete dist directory

--- dist/cjs/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { RequestError } from './function';
--- dist/esm/function.d.ts
+++
@@ -1,9 +0,0 @@
-declare class RequestErrorC extends Error {
-    status: number;
-    response: {
-        message: string;
-    };
-    constructor(message?: string, status?: number);
-}
-declare const RequestError: (message?: string, status?: number) => RequestErrorC;
-export { RequestError };
