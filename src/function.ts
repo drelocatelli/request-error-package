54cd1424648fa5af1de2e37728b430dca6c53f35
Your Name01~ <you@example.com>
2022-06-25 13:37:42 -0300

    refact

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,12 @@
 class RequestErrorC extends Error {
     public status: number = 500;
-    public message: string = 'Internal Server Error';
+    public response: {message: string} = {
+        message: 'Internal Server Error'
+    };
 
     constructor(message?: string, status?: number) {
         super(message);
-        this.message = message ?? this.message;
+        this.response.message = message ?? this.response.message;
         this.status = status ?? this.status;
     }
 }
